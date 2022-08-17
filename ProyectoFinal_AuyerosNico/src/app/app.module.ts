bc89170517037aade2d8316d75fbd4dcb4291dd0
Nico <77641985+niccxlas@users.noreply.github.com>
2022-08-17 19:57:35 -0300

    skills-proyectos-footer

--- ProyectoFinal_AuyerosNico/src/app/app.module.ts
+++ ProyectoFinal_AuyerosNico/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { SkillsComponent } from './components/skills/skills.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +25,9 @@ import { SkillsComponent } from './components/skills/skills.component';
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
-    SkillsComponent
+    SkillsComponent,
+    ProyectosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
