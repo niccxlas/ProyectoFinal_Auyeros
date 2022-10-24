d50052201e066f3d77f356360e4ad8c63427d84c
Nico <77641985+nicoauyeros@users.noreply.github.com>
2022-10-24 10:44:59 -0300

    Implementación database en frontend & routeo

--- ProyectoFinal_AuyerosNico/FRONTEND/src/app/app.module.ts
+++ ProyectoFinal_AuyerosNico/FRONTEND/src/app/app.module.ts
@@ -15,6 +15,9 @@ import { SkillsComponent } from './components/skills/skills.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -28,13 +31,16 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     EducacionComponent,
     SkillsComponent,
     ProyectosComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgCircleProgressModule.forRoot({}),
-    NgbModule
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
